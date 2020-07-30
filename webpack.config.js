0689c567e2edddf9d6ac24f7e6419308939b94cf
Fabio Di Pane <dipane.fabio@gmail.com>
2020-07-30 14:45:00 +0200

    base app configuration

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+	entry: "./src/main.ts",
+	mode: "production",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "build.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: "vue-loader",
+			},
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+				options: { appendTsSuffixTo: [/\.vue$/] },
+			},
+			{
+				test: /\.css$/,
+				use: ["vue-style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: "file-loader",
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".vue", ".json", ".ts"],
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		overlay: true,
+		port: 3000,
+	},
+	performance: {
+		hints: false,
+	},
+	plugins: [new VueLoaderPlugin()],
+};
