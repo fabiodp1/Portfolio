9e392e8d9b83322a74d493aa91a782f4ee68456f
Fabio Di Pane <dipane.fabio@gmail.com>
2020-07-30 16:37:13 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav_bar">
+			<a class="logo" href="#">
+				<img
+					src="/public/favicon.png"
+					alt="F logo"
+					style="width:48px;height:48px;"
+				/>
+			</a>
+			<router-link to="/">Home</router-link>
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view />
+	</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#nav_bar {
+	background: #181818;
+	width: 60px;
+	height: 100%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	z-index: 3;
+	min-height: 500px;
 }
 
-#nav {
-  padding: 30px;
+.logo img {
+	display: block;
+	margin: 8px auto;
+	width: 24px;
+	height: auto;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+	background-color: #1d1d1d;
 }
 </style>
