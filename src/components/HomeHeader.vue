17426f33d4aac8b9e7f95fd15c889f97acf498cd
Fabio dp <dipane.fabio@gmail.com>
2020-08-04 17:35:11 +0200

    more additions on home page

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,41 +1,50 @@
 <template>
-	<div class="home-page">
-		<div class="text-zone">
-			<h1>
-				<span class="blast" style="opacity: 1;">H</span>
-				<span class="blast" style="opacity: 1;">i</span>
-				<span class="blast" style="opacity: 1;">,</span>
-				<br />
-				<span class="blast" style="opacity: 1;">I</span>
-				<span class="blast" style="opacity: 1;">'</span>
-				<span class="blast" style="opacity: 1;">m</span>
-				<img
-					src="@/assets/logo.png"
-					alt="Fabio | Front End Developer"
-					class="animation-logo"
-				/> // TODO modificare logo
-				<span class="blast" aria-hidden="true" style="opacity: 1;">a</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">b</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">i</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">,</span>
-				<br />
-				<span class="blast" aria-hidden="true" style="opacity: 1;">W</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">b</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">d</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">v</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">l</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">p</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">r</span>
-				<span class="blast" aria-hidden="true" style="opacity: 1;">.</span>
-			</h1>
-		</div>
-	</div>
+  <div id="page">
+    <div class="container home-page">
+      <span class="tags top-tags">&nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+      <div class="text-zone">
+        <h1>
+          <span class="blast" style="opacity: 1;">H</span>
+          <span class="blast" style="opacity: 1;">i</span>
+          <span class="blast" style="opacity: 1;">,</span>
+          <br />
+          <span class="blast" style="opacity: 1;">I</span>
+          <span class="blast" style="opacity: 1;">'</span>
+          <span class="blast" style="opacity: 1;">m</span>&nbsp;
+          <img
+            src="@/assets/logoPOnB.png"
+            alt="Fabio | Front End Developer"
+            class="animation-logo"
+          />
+          <span class="blast" aria-hidden="true" style="opacity: 1;">a</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">b</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">i</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">,</span>
+          <br />
+          <span class="blast" aria-hidden="true" style="opacity: 1;">w</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">b</span>&nbsp;
+          <span class="blast" aria-hidden="true" style="opacity: 1;">d</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">v</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">l</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">o</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">p</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">e</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">r</span>
+          <span class="blast" aria-hidden="true" style="opacity: 1;">.</span>
+        </h1>
+        <h2>Front End Developer | Freelancer</h2>
+      </div>
+      <span class="tags">
+        <!-- TODO - fix the spacing -->
+        &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+        <br />&lt;/html&gt;
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -44,45 +53,94 @@ export default {};
 
 <style scoped>
 .home-page .text-zone {
-	position: absolute;
-	left: 10%;
-	top: 50%;
-	transform: translateY(-50%);
-	width: 40%;
-	max-height: 90%;
+  position: absolute;
+  left: 10%;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 40%;
+  max-height: 90%;
 }
 h1 {
-	font-size: 60px !important;
-	display: block;
-	margin-block-start: 0.67em;
-	margin-block-end: 0.67em;
-	margin-inline-start: 0px;
-	margin-inline-end: 0px;
+  font-size: 60px !important;
+  display: block;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+}
+.container {
+  width: 100%;
+  will-change: contents;
+  height: 90%;
+  min-height: 566px;
+  position: absolute;
+  top: 5%;
+  margin: 0 auto;
+  z-index: 1;
+  transform-style: preserve-3d;
 }
 .home-page h1 {
-	color: #fff;
-	line-height: 53px;
-	margin: 0;
-	font-family: "Assistant", sans-serif;
-	font-weight: 400;
+  color: #fff;
+  line-height: 53px;
+  margin: 0;
+  font-family: "Assistant", sans-serif;
+  font-weight: 400;
 }
 .home-page .blast {
-	display: inline-block;
-	transition: all 0.3s ease-out;
+  display: inline-block;
+  transition: all 0.3s ease-out;
 }
 .home-page h1 img {
-	position: relative;
-	margin-left: 0.5em;
-	top: -11px;
-	opacity: 0;
-	width: 32px;
-	height: auto;
-	border: 0;
-	max-width: 100%;
-	vertical-align: middle;
+  position: relative;
+  top: -11px;
+  opacity: 0;
+  width: 32px;
+  height: auto;
+  border: 0;
+  max-width: 100%;
+  vertical-align: middle;
 }
 .animation-logo {
-	animation: animation2 1s linear both;
-	opacity: 1 !important;
+  animation: animation2 1s linear both;
+  opacity: 1 !important;
+}
+.home-page h1:before {
+  content: "<h1>";
+  font-family: "La Belle Aurore", cursive;
+  color: #515152;
+  font-size: 18px;
+  position: absolute;
+  margin-top: -40px;
+  left: -15px;
+}
+.home-page h1:after {
+  content: "<h1/>";
+  font-family: "La Belle Aurore", cursive;
+  color: #515152;
+  font-size: 18px;
+  position: absolute;
+  margin-top: 18px;
+  margin-left: 20px;
+  animation: myanim2 1s 1.7s backwards;
+}
+.home-page h2 {
+  color: #8d8d8d;
+  margin-top: 20px;
+  font-weight: 400;
+  font-size: 11px;
+  font-family: "Open Sans", sans-serif;
+  letter-spacing: 3px;
+}
+.tags {
+  color: #515152;
+  position: absolute;
+  bottom: 0;
+  left: 80px;
+  font-size: 18px;
+  font-family: "La Belle Aurore", cursive;
+}
+.top-tags {
+  bottom: auto;
+  top: 0;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,146 +1,145 @@
 <template>
-	<div id="nav_bar">
-		<a class="logo">
-			<img
-				src="../assets/logo.png"
-				alt="F logo"
-				style="width:25px;height:30px;"
-			/>
-			<img
-				src="../assets/logo_sub.png"
-				alt="FDP | FrontEnd Developer"
-				style="width:30px;height:auto;"
-			/>
-		</a>
-		<nav class="en">
-			<a href="#" rel="index" class="home-link">
-				<i class="fas fa-home icon-home"></i><span>HOME</span>
-			</a>
-			<a href="#" rel="about" class="about-link">
-				<i class="fas fa-user-alt"></i><span>ABOUT</span>
-			</a>
-			<a href="#" rel="skills" class="skills-link">
-				<i class="fas fa-cog"></i><span>SKILLS</span>
-			</a>
-			<a href="#" rel="my-work" class="my-work-link">
-				<i class="far fa-eye"></i><span>WORKS</span>
-			</a>
-			<a href="#" rel="contact" class="contact-link">
-				<i class="far fa-envelope"></i><span>CONTACT</span>
-			</a>
-		</nav>
-		<ul>
-			<li>
-				<a href="https://twitter.com/fabio_dp1" target="_blank"
-					><i class="fab fa-twitter social-icon"></i
-				></a>
-			</li>
-			<li>
-				<a
-					href="https://www.linkedin.com/in/fabio-di-pane-39971222/"
-					target="_blank"
-					><i class="fab fa-linkedin-in social-icon"></i
-				></a>
-			</li>
-			<li>
-				<a href="https://www.facebook.com/dipane.fabio/" target="_blank"
-					><i class="fab fa-facebook-f social-icon"></i
-				></a>
-			</li>
-			<li>
-				<a href="https://github.com/fabiodp1" target="_blank"
-					><i class="fab fa-github-alt social-icon"></i
-				></a>
-			</li>
-		</ul>
-	</div>
+  <div id="nav_bar">
+    <a class="logo">
+      <img src="../assets/logoBOnP.png" alt="F logo" style="width:20px;height:30px;" />
+      <img
+        src="../assets/logo_sub.png"
+        alt="FDP | FrontEnd Developer"
+        style="width:30px;height:auto;"
+      />
+    </a>
+    <nav class="en">
+      <a href="#" rel="index" class="home-link">
+        <i class="fas fa-home icon-home"></i>
+        <span>HOME</span>
+      </a>
+      <a href="#" rel="about" class="about-link">
+        <i class="fas fa-user-alt"></i>
+        <span>ABOUT</span>
+      </a>
+      <a href="#" rel="skills" class="skills-link">
+        <i class="fas fa-cog"></i>
+        <span>SKILLS</span>
+      </a>
+      <a href="#" rel="my-work" class="my-work-link">
+        <i class="far fa-eye"></i>
+        <span>WORKS</span>
+      </a>
+      <a href="#" rel="contact" class="contact-link">
+        <i class="far fa-envelope"></i>
+        <span>CONTACT</span>
+      </a>
+    </nav>
+    <ul>
+      <li>
+        <a href="https://twitter.com/fabio_dp1" target="_blank">
+          <i class="fab fa-twitter social-icon"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/fabio-di-pane-39971222/" target="_blank">
+          <i class="fab fa-linkedin-in social-icon"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/dipane.fabio/" target="_blank">
+          <i class="fab fa-facebook-f social-icon"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/fabiodp1" target="_blank">
+          <i class="fab fa-github-alt social-icon"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 #nav_bar {
-	background: #181818;
-	width: 60px;
-	height: 100%;
-	position: fixed;
-	top: 0;
-	left: 0;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	z-index: 3;
-	min-height: 500px;
+  background: #181818;
+  width: 60px;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 3;
+  min-height: 500px;
 }
 
 #nav_bar nav {
-	display: block;
-	text-align: center;
+  display: block;
+  text-align: center;
 }
 #nav_bar nav a {
-	font-size: 20px;
-	color: #909096;
-	margin-bottom: 15px;
-	display: block;
-	line-height: 51px;
-	height: 51px;
-	position: relative;
-	text-decoration: none;
+  font-size: 20px;
+  color: #909096;
+  margin-bottom: 15px;
+  display: block;
+  line-height: 51px;
+  height: 51px;
+  position: relative;
+  text-decoration: none;
 }
 
 .logo img {
-	display: block;
-	margin: 8px auto;
-	width: 24px;
-	height: auto;
+  display: block;
+  margin: 8px auto;
+  width: 24px;
+  height: auto;
 }
 #nav_bar nav a i {
-	transition: opacity 0.3s ease-in;
-	opacity: 1;
-	height: auto;
+  transition: opacity 0.3s ease-in;
+  opacity: 1;
+  height: auto;
 }
 
 #nav_bar nav a:hover i {
-	opacity: 0;
-	height: 0;
-	overflow: hidden;
+  opacity: 0;
+  height: 0;
+  overflow: hidden;
 }
 #nav_bar nav a span {
-	opacity: 0;
-	height: 0;
-	overflow: hidden;
+  opacity: 0;
+  height: 0;
+  overflow: hidden;
 }
 #nav_bar nav a:hover span {
-	transition: opacity 0.3s ease-in;
-	display: block;
-	color: #fc0893;
-	font-size: 9px;
-	letter-spacing: 2px;
-	position: absolute;
-	bottom: 0;
-	width: 100%;
-	text-align: center;
-	opacity: 1;
-	height: auto;
+  transition: opacity 0.3s ease-in;
+  display: block;
+  color: #fc0893;
+  font-size: 9px;
+  letter-spacing: 2px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  opacity: 1;
+  height: auto;
 }
 
 #nav_bar ul {
-	display: block;
-	padding: 0;
-	list-style: none;
-	text-align: center;
-	margin: 0;
-	padding-bottom: 20px;
+  display: block;
+  padding: 0;
+  list-style: none;
+  text-align: center;
+  margin: 0;
+  padding-bottom: 20px;
 }
 
 #nav_bar ul li a {
-	padding: 7px 0;
-	display: block;
-	font-size: 15px;
-	line-height: 16px;
-	color: #909096;
+  padding: 7px 0;
+  display: block;
+  font-size: 15px;
+  line-height: 16px;
+  color: #909096;
 }
 
 #nav_bar ul li a:hover {
-	transition: color 0.3s ease-in;
-	color: #fc0893;
+  transition: color 0.3s ease-in;
+  color: #fc0893;
 }
 </style>
